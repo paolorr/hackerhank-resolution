fb861de5637d1172b6537a63c23da40664214ac5
Paolo Ramos <paolorr@gmail.com>
2020-08-24 09:52:16 -0300

    Add problem link on index.js

--- problems/birthday-cake-candles/index.js
+++ problems/birthday-cake-candles/index.js
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
 'use strict';
 
 const fs = require('fs');
--- problems/plus-minus/index.js
+++ problems/plus-minus/index.js
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/plus-minus/problem
+
 'use strict';
 
 process.stdin.resume();
